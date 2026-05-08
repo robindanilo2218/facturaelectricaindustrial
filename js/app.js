98c6571abe1034c1849ee59e785cbb525c38e1ca
Robin <robindanilo2218@gmail.com>
2026-05-08 10:46:39 -0600

    V3.2.3 Licencia y quien mantiene el repositorio

--- js/app.js
+++ js/app.js
@@ -15,3 +15,43 @@ window.onload = async () => {
     await loadDataAndRefresh();
     initFileListeners();
 };
+
+let versionClickCount = 0;
+let versionClickTimer = null;
+
+window.handleVersionClick = function() {
+    versionClickCount++;
+    
+    // Reset count if they don't click 3 times within 2 seconds
+    clearTimeout(versionClickTimer);
+    versionClickTimer = setTimeout(() => {
+        versionClickCount = 0;
+    }, 2000);
+
+    if (versionClickCount >= 3) {
+        versionClickCount = 0;
+        showToast("Actualizando a la última versión...");
+        
+        // Unregister service workers and clear caches
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                for(let registration of registrations) {
+                    registration.unregister();
+                }
+            });
+        }
+        
+        if ('caches' in window) {
+            caches.keys().then(function(names) {
+                for (let name of names) {
+                    caches.delete(name);
+                }
+            });
+        }
+        
+        // Reload page from server (bypassing cache)
+        setTimeout(() => {
+            window.location.reload(true);
+        }, 1000);
+    }
+};
